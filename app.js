3c7629a01889e691bdf758a5079c2f4595d91aea
Yoonjieut <qmdldos@gmail.com>
2023-10-06 11:44:08 +0900

    setInterval 시작

---
+++ app.js
@@ -0,0 +1,25 @@
+// setTimeout(callback, delayTime);
+// setTimeout(function(){
+//   console.log('첫 번째');
+// }, 3000);
+// setTimeout(function(){
+//   console.log('두 번째');
+// }, 2000);
+// setTimeout(function(){
+//   console.log('세 번째');
+// }, 1000);
+
+
+// function sosa(){
+
+//   setTimeout(function(){
+//     console.log('sosa');
+//     sosa()
+//   }, 1000);
+
+// }
+// sosa();
+
+setInterval(function(){
+  console.log('향마루 ㄱㄱ?')
+}, 1000);
